21f05ee0697f2f8e1b6a87759cb27f0cd6528684
espinozaerica74 <espinozaerica74@gmail.com>
2020-01-30 02:23:35 +0000

    add input,click handler

--- js/script.js
+++ js/script.js
@@ -1,38 +1,53 @@
-//let songs =["boo'd up"]
-let song = {
+let songs =[
+{  
   title: "boo'd up",
   artist: 'ella mai',
-  length: '300'
-  link: ''
-}
-songs.forEach(function(song){
-$('.songs').append(song);
-})
+  length: '300',
+  link: 'hello'
+},
+{  
+  title: "boo'd up",
+  artist: 'ella mai',
+  length: '300',
+  link: 'hello'
+} 
+]
 
-let artists =["ella mai"]
+$("#add ").click(function(){
+  $("img").hide;
+  let song = $("#add").val();
+});
 
-artists.forEach(function(artist){
-$('.artists').append(artist);
-})
+// Create a working click handler that retrieves user input, pushes new items to your song arrays, and displays the result to the screen
 
-let length =["333"]
+// songs.forEach(function(song){
+// $('.songs').append(song);
+// })
 
-length.forEach(function(length){
-$('.length').append(length);
-})
+// let artists =["ella mai"]
 
-let image =["https://i.ytimg.com/vi/krMG7Xct4Wc/maxresdefault.jpg"]
+// artists.forEach(function(artist){
+// $('.artists').append(artist);
+// })
 
-image.forEach(function(image){
-  $('.image').append(`<img src="${image}">`);
-})
-let link =["https://www.youtube.com/watch?v=6YNZlXfW6Ho"]
+// let length =["333"]
 
-link.forEach(function(link){
-  $('.link').append(`<a href="${link} ">song link </a>`);
-})
+// length.forEach(function(length){
+// $('.length').append(length);
+// })
 
-const count = ["boo'd up"];
-const artist =songs.push();
-console.log(count)
-console.log(artist)
+// let image =["https://i.ytimg.com/vi/krMG7Xct4Wc/maxresdefault.jpg"]
+
+// image.forEach(function(image){
+//   $('.image').append(`<img src="${image}">`);
+// })
+// let link =["https://www.youtube.com/watch?v=6YNZlXfW6Ho"]
+
+// link.forEach(function(link){
+//   $('.link').append(`<a href="${link} ">song link </a>`);
+// })
+
+// const count = ["boo'd up"];
+// const artist =songs.push();
+// console.log(count)
+// console.log(artist)
